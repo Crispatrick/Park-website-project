c3abf18bee12386b23e3e12493e3ad4ab992d4f1
Crispatrick <128227926+Crispatrick@users.noreply.github.com>
2024-02-10 18:32:27 +0800

    first Commit of header

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Rave</title>
+</head>
+<body>
+    <!-- for header -->
+    <?php
+        include("header.html");
+    ?>
+
+    <main>
+
+    </main>
+    
+
+</body>
+</html>
