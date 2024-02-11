d2e9f9091bbedda3e83fab83c08eaf7c68d28d1f
Crispatrick <128227926+Crispatrick@users.noreply.github.com>
2024-02-11 09:31:43 +0800

    testing php and js together

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="global_Palette.css">
     <title>Rave</title>
 </head>
 <body>
@@ -13,7 +14,22 @@
     ?>
 
     <main>
+        <section class="hero">
+            <!-- this is the background -->
+            <video muted autoplay class="hero_Video">
+                <source src="Resources\Videos\RAVE.webm">
+                <source src="Resources\Videos\RAVE_2.webm">
+                <source src="Resources\Videos\RAVE_3.webm">
+                <source src="Resources\Videos\RAVE_4.webm">
+            </video>        
+            
 
+        </section>
+
+
+        <section class="genre">
+            
+        </section>
     </main>
     
 
